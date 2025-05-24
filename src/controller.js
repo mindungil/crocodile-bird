05d3469393768aba00098784f1ea4039f6201efd
soet02 <alswnsrlf12@naver.com>
2025-05-24 21:31:22 +0900

    fix: 기존의 로직 삭제( 확장 프로그램에서 직접 API 통신 X )

--- src/controller.js
+++ src/controller.js
@@ -16,51 +16,6 @@ chrome.runtime.onMessage.addListener(async (msg, sender, sendResponse) => {
     }
 });
 
-// api 요청으로 text 순화처리
-async function cleanText_gpt(text, num, apiKey) {
-    try {
-        // num 번호에 따라 API 요청에 보내는 프롬프트 변경
-        let message;
-        if (num == 1) message = level_1;
-        else if (num == 2) message = level_2;
-        else if (num == 3) message = level_3;
-        else {
-            console.log('프롬프트 번호 수신 오류');
-            // 프롬프트 번호 수신 오류 발생 시 최악의 상황을 방지하여 가장 높은 수준 적용
-            message = prompts.level_3;
-        }
-
-        const res = await fetch(
-            'https://api.openai.com/v1/chat/completions',
-            {
-                method: 'POST',
-                headers: {
-                    Authorization: `Bearer ${apiKey}`,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    model: 'gpt-4o',
-                    messages: [
-                        {
-                            role: 'user',
-                            content: `${message},    변경해야 할 텍스트: ${text}`
-                        }
-                    ]
-            })
-        });
-
-        if (!res.ok) {
-            throw new Error("HTTP 에러, status: ", res.status);
-        }
-        console.log(`Text 처리 성공`);
-        return res.data.choices?.[0]?.message?.content?.trim() || text;
-    } catch(err) {
-        // 기본적인 예외 처리 -> 기본 텍스트 반환
-        console.error(`API 호출 실패: `, err);
-        return text;
-    }
-};
-
 // 서버로 메시지 보내기
 async function cleanText(text, num) {
     try {
@@ -94,7 +49,7 @@ async function cleanText(text, num) {
             }
         );
 
-    return res;
+        return res;
     } catch(err) {
         console.error(err);
     }
